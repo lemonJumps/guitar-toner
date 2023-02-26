e2dfd398ee81af706c242696b2d1e03d12b33c90
Jumping Lemon <lemonjumpsofficial@gmail.com>
2023-02-26 14:57:26 +0100

    Feat: fft sampling

---
+++ toner.py
@@ -0,0 +1,62 @@
+import cv2
+from rapidgui import quickGui
+import pyaudio
+import traceback
+
+import numpy
+
+# chunk = 1024
+#  pyaudio.paFloat32
+ 
+WIDTH = 640
+HEIGHT = 480
+ 
+pad = pyaudio.PyAudio()
+stream = pad.open(
+    format = pyaudio.paInt32,
+    channels = 2,
+    rate = 44100,
+    input=True
+    )
+
+canvas = numpy.zeros((HEIGHT,WIDTH,3), dtype=numpy.uint8)
+fftHist = numpy.zeros((HEIGHT,WIDTH,3), dtype=numpy.uint8)
+
+mmax = 1000
+
+def draw():
+    cv2.imshow("output", canvas)
+    cv2.waitKey(1)
+
+try:
+    while True:
+        data = stream.read(1024)
+
+        fdata = numpy.reshape(numpy.frombuffer(data, dtype=numpy.int32), (-1,2) )
+        
+        
+        cfdata = fdata[:,1]
+        fftdata = numpy.fft.fft(cfdata)
+        
+        fftdata = fftdata.real / numpy.iinfo(numpy.int32).max * (HEIGHT/2) +(HEIGHT/2)
+        
+        canvas = numpy.zeros((HEIGHT,WIDTH,3), dtype=numpy.uint8)
+        
+        fftHist[:,0:WIDTH-1] = fftHist[:,1:WIDTH]
+        
+        # print(fftHist[:,4:WIDTH-1].shape)
+        
+        # mmax = fftdata.real.max()/2 + mmax/2
+        
+        pop = (numpy.abs(fftdata.real[:HEIGHT])/mmax)*255
+        fftHist[:,WIDTH-1] = cv2.applyColorMap(pop.astype(numpy.uint8), colormap=cv2.COLORMAP_DEEPGREEN)[:,0]
+        
+        canvas[:] = fftHist[:]
+        
+        for i in range(len(fftdata)):
+            cv2.circle(canvas, (i,int(fftdata.real[i])), 1, (255,255,255))
+        draw()
+except:
+    print(traceback.format_exc()) 
+
+stream.close()
